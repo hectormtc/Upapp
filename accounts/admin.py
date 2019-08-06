81c56652fbbeeeeb8ed2db925886fcdc9191c75f
Hector Torres <hectormtc@live.com>
2019-08-06 05:13:44 -0600

    Add files via upload

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import User, Connection, Address, Phone
+from .models import User, Connection, Address, Phone, Categoria, Inventario, Articulo, Factura, Rol, Encargado
 
 admin.site.register(User)
 admin.site.register(Connection)
 admin.site.register(Address)
 admin.site.register(Phone)
+admin.site.register(Categoria)
+admin.site.register(Inventario)
+admin.site.register(Articulo)
+admin.site.register(Factura)
+admin.site.register(Rol)
+admin.site.register(Encargado)
